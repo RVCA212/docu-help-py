a6c3c16a478544a2baf809463b4fe73393098e54
RVCA212 <112829052+RVCA212@users.noreply.github.com>
2024-04-28 15:46:27 -0400

    Update index.py

--- index.py
+++ index.py
@@ -3,8 +3,6 @@ app = Flask(__name__)
 from os import environ
 OPENAI_API_KEY=environ.get('OPENAI_API_KEY')
 from os import environ
-ANTHROPIC_API_KEY=environ.get('ANTHROPIC_API_KEY')
-from os import environ
 PINE_API_KEY=environ.get('PINE_API_KEY')
 
 @app.route("/")
@@ -15,206 +13,50 @@ def welcome():
 def home():
     return jsonify({"msg": "Home page here"})
 
-@app.route("/chatting/<string:namespace_name>/<string:q>")
-def chat(namespace_name, q):
-    # self.send_response(200)
-    # self.send_header('Content-type','text/plain')
-    # self.end_headers()
-    from pinecone import Pinecone as PineconeClient
-    import time
-    from langchain_openai import OpenAIEmbeddings
-    from langchain_community.vectorstores import Pinecone
-    from langchain_core.output_parsers import StrOutputParser
-    from langchain_core.prompts import ChatPromptTemplate
-    from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-    from langchain_anthropic import ChatAnthropic
-    # Import Module
-    import json
 
-    
-    model_name = 'text-embedding-ada-002'
-
-    embed = OpenAIEmbeddings(
-        model=model_name,
-        openai_api_key=OPENAI_API_KEY
-    )
-
-#    
-
-    index_name='docu-help'
-
-    # pc = Pinecone(api_key=PINE_API_KEY)
-    pc = PineconeClient(api_key=PINE_API_KEY)
-
-    index = pc.Index(index_name)
-    # wait a moment for connection
-    time.sleep(1)
-
-    index.describe_index_stats()
-
-
-    text_field = "text"
-
-    # switch back to normal index for langchain
-    index = pc.Index(index_name)
-
-    vectorstore = Pinecone(
-        index, embed.embed_query, text_field, namespace=namespace_name
-    )
-    retriever = vectorstore.as_retriever()
-
-
-    # RAG prompt
-    template = """You are an expert software developer who specializes in APIs.  Answer the user's question based only on the following context:
-    {context}
-    Question: {question}
-    """
-    prompt = ChatPromptTemplate.from_template(template)
-
-    model = ChatAnthropic(temperature=0, anthropic_api_key=ANTHROPIC_API_KEY, model_name="claude-3-haiku-20240307")
-
-    def format_docs_with_sources(docs):
-        # This function formats the documents and includes their sources.
-        formatted_docs = []
-        for doc in docs:
-            content = doc.page_content
-            source = doc.metadata.get('source', 'Unknown source')
-            formatted_docs.append(f"{content}\nSource: {source}")
-        return "\n\n".join(formatted_docs)
-
-    rag_chain = (
-        RunnablePassthrough.assign(context=(lambda x: format_docs_with_sources(x["context"])))
-        | prompt
-        | model
-        | StrOutputParser()
-    )
-
-    rag_chain_with_source = RunnableParallel(
-        {"context": retriever, "question": RunnablePassthrough()}
-    ).assign(answer=rag_chain)
-
-    # response = rag_chain_with_source.invoke("Query goes here")
-
-    # response = rag_chain_with_source.invoke("what is langchain")
-
-    response = rag_chain_with_source.invoke(q)
-
-    answer = response['answer']  # Extracting the 'answer' part
-
-    sources = [doc.metadata['source'] for doc in response['context']]
-
-    # formatted_response = f"Answer: {answer}\n\nSources:\n" + "\n".join(sources)
-
-    json_response = {"answer": answer, "sources": sources}
-
-    # print(formatted_response)
-    # self.wfile.write('Hello, world!'.encode('utf-8'))
-    
-    # self.wfile.write(formatted_response.encode('utf-8'))
-    # return
-    # return jsonify({'hello': 'world'})
-    return jsonify(json_response)
-
-
-@app.route("/chat", methods=["POST"])
-def chatting():
+@app.route("/retrieve", methods=["POST"])
+def retrieve():
     requestBody = request.get_json()
     q = requestBody['question']
     namespace_name = requestBody['namespace_name']
-    # self.send_response(200)
-    # self.send_header('Content-type','text/plain')
-    # self.end_headers()
-    from pinecone import Pinecone as PineconeClient
+    # Import necessary modules
     import time
     from langchain_openai import OpenAIEmbeddings
-    from langchain_community.vectorstores import Pinecone
-    from langchain_core.output_parsers import StrOutputParser
-    from langchain_core.prompts import ChatPromptTemplate
-    from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-    from langchain_anthropic import ChatAnthropic
-    # Import Module
+    from pinecone_text.sparse import SpladeEncoder
+    from langchain_community.retrievers import PineconeHybridSearchRetriever
+    from pinecone import Pinecone
     import json
-
-    model_name = 'text-embedding-ada-002'
-
-    embed = OpenAIEmbeddings(
-        model=model_name,
-        openai_api_key=OPENAI_API_KEY
-    )
-
-    index_name='docu-help'
-
-    namespace_name='Langchain'
-
-    # pc = Pinecone(api_key=PINE_API_KEY)
-    pc = PineconeClient(api_key=PINE_API_KEY)
-
-    index = pc.Index(index_name)
-    # wait a moment for connection
-    time.sleep(1)
-
-    index.describe_index_stats()
-
-
-    text_field = "text"
-
-    # switch back to normal index for langchain
-    index = pc.Index(index_name)
-
-    vectorstore = Pinecone(
-        index, embed.embed_query, text_field, namespace=namespace_name
-    )
-    retriever = vectorstore.as_retriever()
-
-
-    # RAG prompt
-    template = """You are an expert software developer who specializes in APIs.  Answer the user's question based only on the following context:
-    {context}
-    Question: {question}
-    """
-    prompt = ChatPromptTemplate.from_template(template)
-
-    # RAG
-    model = ChatAnthropic(temperature=0, anthropic_api_key=ANTHROPIC_API_KEY, model_name="claude-3-haiku-20240307")
-
-    def format_docs_with_sources(docs):
-        # This function formats the documents and includes their sources.
-        formatted_docs = []
-        for doc in docs:
-            content = doc.page_content
-            source = doc.metadata.get('source', 'Unknown source')
-            formatted_docs.append(f"{content}\nSource: {source}")
-        return "\n\n".join(formatted_docs)
-
-    rag_chain = (
-        RunnablePassthrough.assign(context=(lambda x: format_docs_with_sources(x["context"])))
-        | prompt
-        | model
-        | StrOutputParser()
-    )
-
-    rag_chain_with_source = RunnableParallel(
-        {"context": retriever, "question": RunnablePassthrough()}
-    ).assign(answer=rag_chain)
-
-    # response = rag_chain_with_source.invoke("Query goes here")
-
-    # response = rag_chain_with_source.invoke("what is langchain")
-
-    response = rag_chain_with_source.invoke(q)
-
-    answer = response['answer']  # Extracting the 'answer' part
-
-    sources = [doc.metadata['source'] for doc in response['context']]
-
-    # formatted_response = f"Answer: {answer}\n\nSources:\n" + "\n".join(sources)
-
-    json_response = {"answer": answer, "sources": sources}
-
-    # print(formatted_response)
-    # self.wfile.write('Hello, world!'.encode('utf-8'))
     
-    # self.wfile.write(formatted_response.encode('utf-8'))
-    # return
-    # return jsonify({'hello': 'world'})
+    embed = OpenAIEmbeddings(
+        model='text-embedding-3-small',
+        openai_api_key=OPENAI_API_KEY,
+        dimensions = 768
+    )
+    
+    index_name='splade'
+    
+    pc = Pinecone(api_key=PINE_API_KEY)
+    
+    index = pc.Index(index_name)
+    # Wait a moment for connection
+    time.sleep(0.1)
+    
+    splade_encoder = SpladeEncoder()
+    
+    text_field = "text"
+    
+    retriever = PineconeHybridSearchRetriever(
+        embeddings=embed, sparse_encoder=splade_encoder, index=index, namespace=namespace_name, top_k=4
+    )
+
+    results = retriever.invoke(q)
+    # Structure the results into a list of dictionaries with desired keys
+    json_response = [
+        {
+            "page_content": doc.page_content,
+            "source": doc.source
+        } for doc in results
+    ]
+
     return jsonify(json_response)
+
